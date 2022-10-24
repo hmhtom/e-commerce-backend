d283126a0c7bd7368b1cf9384115beb8f5a1049b
hmhtom <hmhtom@gmail.com>
2022-10-24 15:37:34 -0400

    setup model template for database

--- models/index.js
+++ models/index.js
@@ -1,16 +1,34 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Categories have many Products
+Category.hasMany(Product, { foreignKey: "category_id" });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "product_id",
+  otherKey: "tag_id",
+  onDelete: "CASCADE",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "tag_id",
+  otherKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   Product,
